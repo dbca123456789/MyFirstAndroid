86af1222cdba1d6ce5791dc21a34349b55c67748
wxx <921623221@qq.com>
2021-03-22 20:48:28 +0800

    完成注册上传头像功能---1）openAlbum()方法中，new Intent(Intent.ACTION_PICK); 2)Android 10.0及以上需要添加android:requestLegacyExternalStorage=true属性，否则报EACCES (Permission denied)错误

--- app/src/main/java/com/example/myfirstandroid/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/myfirstandroid/Activity/RegisterActivity.java
@@ -1,17 +1,154 @@
 
 package com.example.myfirstandroid.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myfirstandroid.R;
+import com.example.myfirstandroid.Utils.AlbumUtil;
+import com.example.myfirstandroid.Utils.ApplicationUtil;
+import com.example.myfirstandroid.Utils.MyDatabaseHelper;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private TextView submit;
+    private ImageView uploadHead;
+    private EditText username, password, repassword;
+    private CheckBox checkRules;
+
+    private MyDatabaseHelper dbHelper;
+
+    private static final int CHOOSE_PHOTO = 1;
+    private static final String TAG = "RegisterActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        dbHelper = new MyDatabaseHelper(this, "UserDB.db", null, 1);
+
+        submit = findViewById(R.id.submit_info);
+        uploadHead = findViewById(R.id.upload_head);
+        username = findViewById(R.id.register_username);
+        password = findViewById(R.id.register_password);
+        repassword = findViewById(R.id.register_repassword);
+
+        checkRules = findViewById(R.id.check_rules);
+
+        uploadHead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(RegisterActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(RegisterActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                }else {
+                    openAlbum();
+                }
+            }
+
+        });
+
+        ApplicationUtil.getInstance().addActivity(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    openAlbum();
+                }else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    private void openAlbum() {
+
+        boolean flag = isExternalStorageWritable();
+        Log.i(TAG, "外存是否可读写：" + flag);
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i(TAG, "图片封装后的url："+data.toString());  // Intent { dat=content://media/external/images/media/30 flg=0x1 }
+        switch (requestCode) {
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    String imagePath;
+                    Log.i(TAG, "Build.VERSION.SDK_INT："+Build.VERSION.SDK_INT);
+                    if (Build.VERSION.SDK_INT >= 19){
+                        imagePath = AlbumUtil.handleImageOnKitKat(this, data);
+                    }else {
+                        Log.i(TAG, "SDK_INT < 19, 系统版本4.4以下");
+                        imagePath = AlbumUtil.handleImageBeforeKitKat(this, data);
+                    }
+                    setHead(imagePath);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void setHead(String imagePath) {
+        if (imagePath != null){
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            Bitmap round = AlbumUtil.toRoundBitmap(bitmap);
+            try {
+                String path = getCacheDir().getPath();
+                File file = new File(path, "user_head");
+                Log.i(TAG, "file路径"+file.toString());
+                round.compress(Bitmap.CompressFormat.JPEG, 100, new FileOutputStream(file));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            uploadHead.setImageBitmap(round);
+        }else {
+            Toast.makeText(this, "failed to get image", Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/example/myfirstandroid/Utils/MyDatabaseHelper.java
@@ -0,0 +1,44 @@
+package com.example.myfirstandroid.Utils;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private Context mContext;
+
+    public static final String CREATE_USER = "create table user("
+            + "id integer primary key autoincrement,"
+            + "name text,"
+            + "password text)";
+    public static final String CREATE_COLLECTION_NEWS = "create table collection_news("
+            + "id integer primary key autoincrement,"
+            + "news_title text,"
+            + "news_date text,"
+            + "news_author text,"
+            + "news_picurl text,"
+            + "news_url text)";
+
+    private static final String TAG = "MyDatabaseHelper";
+
+    public MyDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_USER);
+        db.execSQL(CREATE_COLLECTION_NEWS);
+        Log.i(TAG, "Created DataTable Succeed!");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
